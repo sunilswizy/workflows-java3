ec22caf9e39a8d4cc9370e1d6521ee46cea9933b
sunil_swizy <sunil@iamneo.ai>
2023-06-14 13:47:23 +0530

    commit

--- src/main/java/com/examly/springapp/SwapNumbers.java
+++ src/main/java/com/examly/springapp/SwapNumbers.java
@@ -8,13 +8,10 @@ public class SwapNumbers {
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
 
-        // Value of first is assigned to temporary
         float temporary = first;
 
-        // Value of second is assigned to first
         first = second;
 
-        // Value of temporary (which contains the initial value of first) is assigned to second
         second = temporary;
 
         System.out.println("--After swap--");
